d2535ace1474c45c15cf20e4c41abdc255c5403b
Viktoras Domarkas <vidomarkas@gmail.com>
2019-09-14 17:25:58 +0100

    takes an array of arrays, but returns array of objects

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function checkCashRegister(price, cash, cid) {
-  cid = cid.reverse();
-  let changeDue = cash - price;
-  console.log("changeDue :", changeDue);
+  const newCid = [],
+    change = [];
 
-  const totalCash = cid => {
+  let totalCash, changeDue;
+
+  //round number to two decimals
+  const roundedToTwoDecimals = element => {
+    return Math.round(element * 100) / 100;
+  };
+
+  //convert the cash-in-drawer to a more manageable data structure
+  const convertCid = () => {
+    //add value to each denomination
+    cid[0][2] = 0.01;
+    cid[1][2] = 0.05;
+    cid[2][2] = 0.1;
+    cid[3][2] = 0.25;
+    cid[4][2] = 1;
+    cid[5][2] = 5;
+    cid[6][2] = 10;
+    cid[7][2] = 20;
+    cid[8][2] = 100;
+
+    cid.forEach(el => {
+      newCid.unshift(
+        new Object({
+          name: el[0],
+          amount: Math.round(el[1] / el[2]),
+          value: el[2]
+        })
+      );
+    });
+  };
+
+  convertCid(cid);
+
+  //calculate total cash amount in register
+  const calcTotalCash = newCid => {
     totalSum = 0;
-    cid
+    newCid
       .map(value => {
         return value[1];
       })
@@ -20,92 +53,59 @@ function checkCashRegister(price, cash, cid) {
     return totalSum;
   };
 
-  if (changeDue > totalCash(cid)) {
-    console.log("line 23: insufficient funds");
-    return null;
-  }
+  //calculate initial due change
+  changeDue = roundedToTwoDecimals(cash - price);
 
   const calculateChange = changeDue => {
-    // usedDenominationArray = [];
     let usedDenomination;
-    change = [];
 
-    // 1.gauti nominalus, kurie yra maziau uz change ir prideti prie naudotinu nominalu
-    // cid.some(element => {
-    //   if (changeDue > element.value && element.amount > 1) {
-    //     // usedDenominationArray.unshift(element);
-    //     usedDenomination = Object.create(element);
-    //     element.amount--;
-    //     console.log(element);
-    //     console.log("hey", usedDenomination);
-    //   } else {
-    //     console.log("line 39: insufficient funds");
-    //     return null;
-    //   }
-    // });
-    cid.some(element => {
-      if (changeDue > element.value && element.amount > 1) {
-        // usedDenominationArray.unshift(element);
-        usedDenomination = Object.create(element);
+    usedDenomination = newCid.find(element => {
+      if (element.value <= changeDue && element.amount > 1) {
         element.amount--;
-        console.log(element);
         return element;
-        console.log("hey", usedDenomination);
-      } else {
-        console.log("line 39: insufficient funds");
-        return null;
       }
     });
 
-    // 2.is grazos atimame nominala
-    changeDue = changeDue - usedDenomination.value;
-    // 3.prie grazos array pridedame panaudota nominala
-    change.push(usedDenomination);
+    if (usedDenomination) {
+      changeDue = roundedToTwoDecimals(changeDue - usedDenomination.value);
+      console.log("changeDue", changeDue);
+      change.push(usedDenomination);
+    } else {
+      changeDue = -1;
+    }
 
-    if (changeDue !== 0) {
+    if (changeDue === calcTotalCash(newCid)) {
+      console.log(`{status: "CLOSED", change: [...]}`);
+      return { status: "CLOSED", change };
+    } else if (changeDue > 0 && changeDue !== totalCash) {
       return calculateChange(changeDue);
     } else if (changeDue === 0) {
-      //grazinti change array su esanciais nominalais.
-      console.log("change: 0", change);
+      console.log(`{ status: "CLOSED", change }`, change);
       return { status: "CLOSED", change };
+    } else if (changeDue === -1) {
+      console.log(`{ status: "INSUFFICIENT_FUNDS", change }`, {
+        status: "INSUFFICIENT_FUNDS",
+        change: []
+      });
+      return { status: "INSUFFICIENT_FUNDS", change: [] };
     }
-    // 4.griztame kartoti 1, 2 zingsnius
   };
 
   calculateChange(changeDue);
 }
 
-// checkCashRegister(19.5, 20, [
-//   ["PENNY", 1.01],
-//   ["NICKEL", 2.05],
-//   ["DIME", 3.1],
-//   ["QUARTER", 4.25],
-//   ["ONE", 90],
-//   ["FIVE", 55],
-//   ["TEN", 20],
-//   ["TWENTY", 60],
-//   ["ONE HUNDRED", 100]
-// ]);
-checkCashRegister(19.5, 20, [
-  { name: "PENNY", amount: 101, value: 0.01 },
-  { name: "NICKEL", amount: 45, value: 0.05 },
-  { name: "DIME", amount: 31, value: 0.1 },
-  { name: "QUARTER", amount: 17, value: 0.25 },
-  { name: "ONE", amount: 90, value: 1 },
-  { name: "FIVE", amount: 11, value: 5 },
-  { name: "TEN", amount: 2, value: 10 },
-  { name: "TWENTY", amount: 3, value: 20 },
-  { name: "ONE HUNDRED", amount: 1, value: 100 }
-]);
-
-// cid = [
-//     {name:"PENNY",       amount:101, value: 0.01},
-//     {name:"NICKEL",      amount:45,  value: 0.05},
-//     {name:"DIME",        amount:31,  value: 0.1},
-//     {name:"QUARTER",     amount:17,  value: 0.25},
-//     {name:"ONE",         amount:90,  value: 1},
-//     {name:"FIVE",        amount:11,  value: 5},
-//     {name:"TEN",         amount:2,   value: 10},
-//     {name:"TWENTY",      amount:3,   value: 20},
-//     {name:"ONE HUNDRED", amount:1,   value: 100},
-// ]
+checkCashRegister(
+  18.45,
+  20,
+  (cid = [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ])
+);
